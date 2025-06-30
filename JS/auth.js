c2a61792878a23fa09dde5089925978d4adb465c
lakoth <lornaakothpb@gmail.com>
2025-06-30 14:01:34 +0300

    add:getting jwt token

--- JS/auth.js
+++ JS/auth.js
@@ -1,15 +1,42 @@
-import {email,password} from "./elements.js"
-import {displayError} from "./error.js";
-import{Endpoints} from "./constants.js"
+import { email, password } from "./elements.js"
+import { displayError } from "./error.js";
+import { Endpoints } from "./constants.js"
 
 
-const doLogin=async()=>{
-try{
-    const response=await fetch(Endpoints.SIGNIN_ENDPOINT,{
+export const doLogin = async () => {
+    try {
+        if (!email || !password) {
+            displayError("All fields are required", 400)
+            return
+        }
+        const response = await fetch(Endpoints.SIGNIN_ENDPOINT, {
+            method: "POST",
+            headers: {
+                'Authorization': `Basic${btoa(`${email}:${password}`)}`,
+                'Content-Type': 'application/json'
+            },
+        })
 
-    })
-
-}catch(error){
-
+        if (response.ok) {
+            const jwt = await response.json();
+            console.log(jwt);
+            localStorage.setItem('token', jwt);
+        } else {
+            if (email.includes('@')) {
+                displayError("Email or password is incorrect", response.status);
+                return
+            }
+            displayError("Username or password is incorrect", response.status);
+            return;
+        }
+    } catch (error) {
+        displayError("Error getting access token", error);
+        return
+    }
 }
+
+export const doLogOut = async () => {
+    localStorage.removeItem('token')
+    //redirect to login window.location.href="/login"
+
 }
