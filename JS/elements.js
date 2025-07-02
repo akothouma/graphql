57c67e5b65337e3a813adda11be32b1811ec717c
lakoth <lornaakothpb@gmail.com>
2025-07-02 18:01:12 +0300

    add:elements to dom

--- JS/elements.js
+++ JS/elements.js
@@ -2,14 +2,18 @@ export const Elements = () => {
     const emailElement = document.getElementById("email/username");
     const passwordElement = document.getElementById("password");
     const dashboard = document.getElementById("pageLayout");
-    const loginpage = document.getElementById("login")
+    const loginPage= document.getElementById("loginPage")
+    const loginButton= document.getElementById("login")
+    const error=document.getElementById("errorDisplay")
     const logoutButton=document.getElementById("logout")
 
     return {
-        email: emailElement ? emailElement.value : "",
-        password: passwordElement ? passwordElement.value : "",
+        email: emailElement ? emailElement.value.trim() : "",
+        password: passwordElement ? passwordElement.value.trim() : "",
         logoutButton,
         dashboard,
-        loginpage
+        loginButton,
+        loginPage,
+        error
     }
 }
