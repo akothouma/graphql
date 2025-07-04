a1fd60283f8fe13a3e9b2ee97e8a830dc0d99690
lakoth <lornaakothpb@gmail.com>
2025-07-04 20:05:01 +0300

    updating UI call on queryAPI

--- JS/query.js
+++ JS/query.js
@@ -1,23 +1,25 @@
 import { Endpoints } from "./constants.js"
 import { displayError } from "./error.js"
+import { updateUI } from "./profile.js"
 
-export const queryApi=async()=>{
-    try{
-        const jwt=localStorage.getItem('token')
-        const response= await fetch(Endpoints.GRAPHQL_ENPOINT,{
-        method:'POST',
-        headers:{
-            'Content-Type':'application/json',
-            Authorization:`Bearer ${jwt} `
-        },
-        body:JSON.stringify({query:Endpoints.QUERY_STRING})
-       })
+export const queryApi = async () => {
+  try {
+    const jwt = localStorage.getItem('token')
+    const response = await fetch(Endpoints.GRAPHQL_ENPOINT, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${jwt} `
+      },
+      body: JSON.stringify({ query: Endpoints.QUERY_STRING })
+    })
 
-       const result=await response.json()
-    
-    }
-    catch(error){
-      displayError("Error getting profile",error)
-    }
+    const result = await response.json();
+    console.log(result)
+    updateUI(result.data)
+  }
+  catch (error) {
+    displayError("Error getting profile", error)
+  }
 
 }
