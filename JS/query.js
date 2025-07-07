3909044bbd00e5bbbcc4eb00191f8cad46630f42
lakoth <lornaakothpb@gmail.com>
2025-07-07 16:26:40 +0300

    fix:handling token expiring time

--- JS/query.js
+++ JS/query.js
@@ -1,10 +1,16 @@
 import { Endpoints } from "./constants.js"
 import { displayError } from "./error.js"
 import { updateUI } from "./profile.js"
+import {showLogin} from "./index.js"
 
 export const queryApi = async () => {
   try {
     const jwt = localStorage.getItem('token')
+    if(TokenExpired(jwt)){
+      localStorage.removeItem('token')
+      showLogin();
+      return;
+    }
     const response = await fetch(Endpoints.GRAPHQL_ENPOINT, {
       method: 'POST',
       headers: {
@@ -15,11 +21,22 @@ export const queryApi = async () => {
     })
 
     const result = await response.json();
-    console.log(result)
     updateUI(result.data)
   }
   catch (error) {
     displayError("Error getting profile", error)
   }
 
+}
+
+function TokenExpired(token){
+  if(!token) return true;
+  try{
+    const payload=JSON.parse(atob(token.split('.')[1]))
+    const exp=payload.exp;
+    const now=Date.now()/1000;
+    return now>exp;
+  }catch{
+    return true;
+  }
 }
